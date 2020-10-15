f5f88b2cd2ffcf5213fa69fc68ced69670432d51
sbmallik <smallik@gannett.com>
2020-10-15 12:08:35 -0400

    Fix the unmute and mute test.

--- test/scripts/unmute-and-mute.video.test.js
+++ test/scripts/unmute-and-mute.video.test.js
@@ -12,7 +12,7 @@ describe('teal player test', () => {
     await driver.get(global.baseUrl + '/videos/news/have-you-seen/2020/09/15/firefighters-saved-trapped-puppy-underneath-owners-recliner/5804308002/?gnt-test-navpromo&gnt-test-alert=off&gnt-hostname-override=www.usatoday.com&gnt-test-navigation#gnt-disable-x&gnt-disable-videoAds&gnt-disable-taboola');
   });
     
-  testName = test.skip('mute-unmute-video', async() => {
+  testName = test('unmute-mute-video', async() => {
     // Hover on the video elememnt to ensure control bar display
     const playToggle = await driver.findElement(By.css('.tealplayer-play-toggle'));
     await driver.actions({
@@ -23,21 +23,21 @@ describe('teal player test', () => {
       x: 0,
       y: 0
     }).perform();
-    // Wait for the pause button to display after video starts
-    const pauseButton = await driver.wait(until.elementLocated(By.css('#unmuteTealSvg')), global.ELEMENT_TIMEOUT);
-    await driver.wait(until.elementIsVisible(pauseButton), global.ELEMENT_TIMEOUT);
-    // Click the puase button
+    // Wait for the unmute button to display after video starts
+    const unmuteButton = await driver.wait(until.elementLocated(By.css('#unmuteTealSvg')), global.ELEMENT_TIMEOUT);
+    await driver.wait(until.elementIsVisible(unmuteButton), global.ELEMENT_TIMEOUT);
+    // Click the unmute button
     await driver.findElement(By.css('#unmuteTealSvg')).click();
-    // Validate the pause button is hidden
+    // Validate the unmute button is hidden
     await driver.wait(until.elementLocated(By.css('.tealplayer-volume-wrap .btn-index-1.tealplayer-vis-hidden')), global.ELEMENT_TIMEOUT);
     // Pause for a second
     await driver.sleep(1000);
-    // Wait for the play button to display after video starts
-    const playButton = await driver.wait(until.elementLocated(By.css('#muteTealSvg')), global.ELEMENT_TIMEOUT);
-    await driver.wait(until.elementIsVisible(playButton), global.ELEMENT_TIMEOUT);
-    // Click the play button
+    // Wait for the mute button to display after video starts
+    const muteButton = await driver.wait(until.elementLocated(By.css('#muteTealSvg')), global.ELEMENT_TIMEOUT);
+    await driver.wait(until.elementIsVisible(muteButton), global.ELEMENT_TIMEOUT);
+    // Click the mute button
     await driver.findElement(By.css('#muteTealSvg')).click();
-    // Validate the play button is hidden
+    // Validate the mute button is hidden
     await driver.wait(until.elementLocated(By.css('.tealplayer-volume-wrap .btn-index-0.tealplayer-vis-hidden')), global.ELEMENT_TIMEOUT);
   });
     
